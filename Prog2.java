9aecdd9cfc5927b739ab767b6038bfe0abda47bb
Le <ale352@student.cccd.edu>
2022-05-11 10:37:52 -0700

    New Feature added

--- Prog2.java
+++ Prog2.java
@@ -3,5 +3,10 @@ public class Prog2
 	public static void main(String[] args)
 	{
 		System.out.println("Hi");
+		coolFeature();
+	}
+	
+	public static void coolFeature(){
+		System.out.println("Pretty cool, eh?");
 	}
 }
